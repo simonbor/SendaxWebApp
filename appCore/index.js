cffd19385d029f0dfadb2313914ec6c9ef95a513
simonbor <simonbor@gmail.com>
2017-01-22 22:18:40 +0200

    App Config ready

--- appCore/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./BaseProvider"));
-__export(require("./Factory"));
-__export(require("./Dal"));
-__export(require("./Sender"));
-//# sourceMappingURL=index.js.map
